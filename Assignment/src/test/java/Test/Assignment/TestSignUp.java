ebe6155df1f0a31ca113a0fa64f986d91bd595d6
Ganesh-Dhurde <97847934+Ganesh-Dhurde@users.noreply.github.com>
2022-03-01 16:28:12 +0530

    Add files via upload

---
+++ Assignment/src/test/java/Test/Assignment/TestSignUp.java
@@ -0,0 +1,11 @@
+package Test.Assignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSignUp {
+
+	
+	System.setProperty("webdriver.chrome.driver", "F:\\Selenium Data\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+}
